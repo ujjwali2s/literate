4a0fa0a3158caeae04be10e1652899b000334ebb
Ujjwal kumar <149386327+ujjwali2s@users.noreply.github.com>
2024-12-11 15:09:47 +0530

    Update ScoreCard.tsx

--- frontend/src/scorecard/ScoreCard.tsx
+++ frontend/src/scorecard/ScoreCard.tsx
@@ -11,7 +11,7 @@ import FallOfWickets from './FallOfWickets'; // Import the FallOfWickets compone
 import ExtrasSummary from './extraSummry'; // Import the ExtrasSummary component
 
 export function ScoreCard() {
-  const Baseurl ="http://3.110.27.69:5000/api"
+  const Baseurl ="/api"
   const { matchId } = useParams();
   const [score, setScore] = useState(null);
   const [odds, setOdds] = useState(null);
