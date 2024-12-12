3c0c7077e71976e940cc6192a7ec3b2377abdea2
Ujjwal kumar <149386327+ujjwali2s@users.noreply.github.com>
2024-12-12 10:31:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "OK", message: "jhat jal raha ab" });
+  res.status(200).json({ status: "OK", message: "front nhi kr raha work" });
 });
 
 
